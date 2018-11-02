d402e49c54bdd07ccea34253c4657754c5914ec1
ibrahimnsu15 <ibrahimcoxs11@gmail.com>
2018-11-03 02:00:43 +0600

    create server and add some folder

---
+++ main.js
@@ -0,0 +1,29 @@
+
+// all variable
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000; // server port
+
+// static 
+app.use(express.static(__dirname+"/resource"));
+
+
+// Middleware
+const middleware = (req,res,next)=>{
+    console.log(req.method +" "+req.url);   
+    next();
+}
+app.use(middleware);
+
+app.route("/")
+    .get( (req,res) =>{
+        res.send("hello from app");
+    });
+
+
+app.listen(port, console.log(`Server start on port no : ${port}`));
+
+
+
+
+
