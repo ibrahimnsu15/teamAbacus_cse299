30cdc872a3b295efba8843b26aab356c0a753a2a
ibrahimnsu15 <ibrahimcoxs11@gmail.com>
2018-12-06 14:04:42 +0600

    fixed time bug

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ function getTime(){
 
     var time  = new Date();
 
-    var hour = time.getHours()+6;
+    var hour = time.getHours();
     var minute = time.getMinutes();
     var second = time.getSeconds();
     var meridian="am"
